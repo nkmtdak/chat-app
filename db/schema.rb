6322da935a3e325ec5e8d829f08d454d5e8ca68b
nkmtdak <tanemaki@hotmail.com>
2024-07-30 23:07:26 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_30_052159) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_30_121527) do
   create_table "rooms", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
